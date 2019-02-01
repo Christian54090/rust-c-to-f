788f2c3a4abd068cd769584ef5f4cb62796fb1f9
Christian Sanders <christian.sanders@live.com>
2019-01-31 18:01:54 -0800

    Converter finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+
+}
+
+fn c_to_f(num: i32) -> i32 {
+    num * 2 + 30
+}
+
+fn f_to_c(num: i32) -> i32 {
+    (num - 30) / 2
+}
+
+#[test]
+fn test_c_to_f() {
+    assert_eq!(52, c_to_f(11));
+}
+
+#[test]
+fn test_f_to_c() {
+    assert_eq!(19, f_to_c(68));
 }
